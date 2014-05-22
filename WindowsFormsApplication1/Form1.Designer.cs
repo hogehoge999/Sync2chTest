ffcf8c54fcf26f5122e22758d5b4e11baec321dc
hogehoge999 <hogehoge@masa.dtdns.net>
2014-05-22 15:43:59 +0900

    Initial

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUserID = new System.Windows.Forms.TextBox();
+            this.textBoxPasscode = new System.Windows.Forms.TextBox();
+            this.textBoxXML = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonMakeXML = new System.Windows.Forms.Button();
+            this.textBoxClientID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxUserID
+            // 
+            this.textBoxUserID.Location = new System.Drawing.Point(63, 25);
+            this.textBoxUserID.Name = "textBoxUserID";
+            this.textBoxUserID.Size = new System.Drawing.Size(159, 19);
+            this.textBoxUserID.TabIndex = 0;
+            // 
+            // textBoxPasscode
+            // 
+            this.textBoxPasscode.Location = new System.Drawing.Point(64, 50);
+            this.textBoxPasscode.Name = "textBoxPasscode";
+            this.textBoxPasscode.Size = new System.Drawing.Size(158, 19);
+            this.textBoxPasscode.TabIndex = 1;
+            // 
+            // textBoxXML
+            // 
+            this.textBoxXML.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxXML.Location = new System.Drawing.Point(0, 0);
+            this.textBoxXML.Multiline = true;
+            this.textBoxXML.Name = "textBoxXML";
+            this.textBoxXML.Size = new System.Drawing.Size(579, 197);
+            this.textBoxXML.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonMakeXML);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxUserID);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxClientID);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxPasscode);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxXML);
+            this.splitContainer1.Size = new System.Drawing.Size(579, 394);
+            this.splitContainer1.SplitterDistance = 193;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "pass";
+            // 
+            // buttonMakeXML
+            // 
+            this.buttonMakeXML.Location = new System.Drawing.Point(12, 167);
+            this.buttonMakeXML.Name = "buttonMakeXML";
+            this.buttonMakeXML.Size = new System.Drawing.Size(168, 23);
+            this.buttonMakeXML.TabIndex = 4;
+            this.buttonMakeXML.Text = "MakeXML";
+            this.buttonMakeXML.UseVisualStyleBackColor = true;
+            this.buttonMakeXML.Click += new System.EventHandler(this.buttonMakeXML_Click);
+            // 
+            // textBoxClientID
+            // 
+            this.textBoxClientID.Location = new System.Drawing.Point(63, 103);
+            this.textBoxClientID.Name = "textBoxClientID";
+            this.textBoxClientID.Size = new System.Drawing.Size(158, 19);
+            this.textBoxClientID.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ClientID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Get ClientID";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(330, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Request";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 394);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUserID;
+        private System.Windows.Forms.TextBox textBoxPasscode;
+        private System.Windows.Forms.TextBox textBoxXML;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonMakeXML;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxClientID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+class StringWriterUTF8 : System.IO.StringWriter
+{
+    public override Encoding Encoding
+    {
+        get { return Encoding.UTF8; }
+    }
+}
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxUserID.Text = Properties.Settings.Default.UserID;
+            textBoxPasscode.Text = Properties.Settings.Default.Passcode;
+            textBoxClientID.Text = Properties.Settings.Default.ClientID;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxClientID.Text.Length > 0)
+            {
+                client_id = textBoxClientID.Text;
+            }
+            else
+            {
+                string username = textBoxUserID.Text;
+                string passcode = textBoxPasscode.Text;
+
+
+                System.Net.HttpWebRequest webreq = (System.Net.HttpWebRequest)
+                    System.Net.WebRequest.Create("https://sync2ch.com/api/auth2");
+
+                //認証の設定
+                webreq.Credentials =
+                    new System.Net.NetworkCredential(username, passcode);
+
+                //HttpWebResponseの取得
+                System.Net.HttpWebResponse webres =
+                    (System.Net.HttpWebResponse)webreq.GetResponse();
+
+                string[] values = webres.Headers.GetValues("Sync2ch-Client-ID");
+                textBoxClientID.Text = values[0];
+                client_id = values[0];
+                //受信して表示
+                System.IO.Stream st = webres.GetResponseStream();
+                System.IO.StreamReader sr = new System.IO.StreamReader(st);
+                Console.WriteLine(sr.ReadToEnd());
+                //閉じる
+                sr.Close();
+                st.Close();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.UserID = textBoxUserID.Text;
+            Properties.Settings.Default.Passcode = textBoxPasscode.Text;
+            Properties.Settings.Default.ClientID = textBoxClientID.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        int sync_number = 0;
+        string client_id = "";
+        private void buttonMakeXML_Click(object sender, EventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+
+            XmlDeclaration declaration = document.CreateXmlDeclaration("1.0", "UTF-8", null);  // XML宣言
+            XmlElement sync2ch_request = CreateRequest(document, sync_number, client_id);
+
+            document.AppendChild(declaration);
+            document.AppendChild(sync2ch_request);
+
+            XmlElement thread_group = document.CreateElement("thread_group");
+            thread_group.SetAttribute("category", "favorite");
+            //thread_group.SetAttribute("category", "open");
+            //thread_group.SetAttribute("category", "favorite");
+            //thread_group.SetAttribute("category", "history");
+            //thread_group.SetAttribute("category", "post_history");
+
+            XmlElement dir = document.CreateElement("dir");
+            dir.SetAttribute("name", "フォルダ1");
+
+            thread_group.AppendChild(dir);
+
+            XmlElement th = CreateThread(document);
+            dir.AppendChild(th);
+
+            sync2ch_request.AppendChild(thread_group);
+
+            // ファイルに保存する
+            //document.Save("sample.xml");
+            //textBoxXML.Text = document.OuterXml;
+            // StringWriterをつくる
+            StringWriter writer = new StringWriter();
+            // StringWriterに整形済みXMLを書き出し
+            document.Save(writer);
+            // 文字列として書き出し
+            textBoxXML.Text = writer.ToString();
+            // StringWriterを閉じる
+            writer.Close();
+            sync_number++;
+        }
+        /// <summary>
+        /// 3.2 ルート要素
+        ///ルート要素<sync2ch_request>には以下の属性を指定します。
+
+        ///sync_number(必須) ・・・ 同期番号(初回同期時には0）を指定
+        ///client_id ・・・ クライアント認証プロセスによって得たクライアントID
+        ///client_name ・・・ クライアント名
+        ///client_version ・・・ クライアントバージョン
+        ///os ・・・ OSを表す文字列
+        ///device ・・・ 機種名（スマートフォン等の機種名を指定します。)
+        ///sync_rl ・・・ 同期するレス番号リストのカテゴリ名をコンマ区切りで指定します(rlはRes Listの略)。
+        /// </summary>
+        /// <param name="document"></param>
+        /// <param name="syncNumber"></param>
+        /// <param name="clientID"></param>
+        /// <returns></returns>
+        XmlElement CreateRequest(XmlDocument document, int syncNumber, string clientID)
+        {
+            XmlElement sync2ch_request = document.CreateElement("sync2ch_request");
+            sync2ch_request.SetAttribute("sync_number", syncNumber.ToString());
+            sync2ch_request.SetAttribute("client_id", clientID);
+            sync2ch_request.SetAttribute("sync_rl", "post,re");
+            sync2ch_request.SetAttribute("client_version", "0.0.1");
+            sync2ch_request.SetAttribute("client_name", "Thousand Sync");
+            sync2ch_request.SetAttribute("os", "Windows 7");
+            sync2ch_request.SetAttribute("device", "PC");
+            return sync2ch_request;
+        }
+        /// <summary>
+        /// <th> ・・・スレッド 属性: url, title, read, now, count, rt, pt, rl_post (rl_{CATEGORY})</th>
+        /// </summary>
+        /// <param name="document"></param>
+        /// <returns></returns>
+        XmlElement CreateThread(XmlDocument document)
+        {
+            XmlElement th = document.CreateElement("th");
+            th.SetAttribute("url", "http://toro.2ch.net/read.cgi/test/sampleboard/1389394832/");
+            th.SetAttribute("title", "サンプルスレッド");
+            th.SetAttribute("read", "32");
+            th.SetAttribute("now", "32");
+            th.SetAttribute("count", "523");
+            return th;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            string username = textBoxUserID.Text;
+            string passcode = textBoxPasscode.Text;
+
+            XmlDocument document = new XmlDocument();
+
+            XmlDeclaration declaration = document.CreateXmlDeclaration("1.0", "UTF-8", null);  // XML宣言
+            XmlElement sync2ch_request = CreateRequest(document, sync_number, client_id);
+            document.AppendChild(declaration);
+            document.AppendChild(sync2ch_request);
+
+            XmlElement thread_group = document.CreateElement("thread_group");
+            thread_group.SetAttribute("category", "favorite");
+            XmlElement dir = document.CreateElement("dir");
+            dir.SetAttribute("name", "フォルダ1");
+            thread_group.AppendChild(dir);
+            XmlElement th = CreateThread(document);
+            dir.AppendChild(th);
+
+            sync2ch_request.AppendChild(thread_group);
+
+            StringWriterUTF8 writer = new StringWriterUTF8();
+            document.Save(writer);
+            string xmlString = writer.ToString();
+            writer.Close();
+            byte[] postDataBytes = Encoding.UTF8.GetBytes(xmlString);
+
+            System.Net.HttpWebRequest webreq = (System.Net.HttpWebRequest)
+                System.Net.WebRequest.Create("http://sync2ch.com/api/sync2");
+
+            //認証の設定
+            webreq.Credentials =
+                new System.Net.NetworkCredential(username, passcode);
+            
+            System.Net.ServicePointManager.Expect100Continue = false;
+
+            webreq.Method = "POST";
+            webreq.ContentType = "application/x-www-form-urlencoded";
+            webreq.ContentLength = postDataBytes.Length;
+            //HttpWebResponseの取得
+            Stream reqStream = webreq.GetRequestStream();
+            reqStream.Write(postDataBytes, 0, postDataBytes.Length);
+            reqStream.Close();
+
+            System.Net.HttpWebResponse webres =
+                (System.Net.HttpWebResponse)webreq.GetResponse();
+
+            //受信して表示
+            System.IO.Stream st = webres.GetResponseStream();
+            System.IO.StreamReader sr = new System.IO.StreamReader(st);
+            String result = sr.ReadToEnd();
+            textBoxXML.Text = result;
+            //閉じる
+            sr.Close();
+            st.Close();
+        }
+    }
+}
